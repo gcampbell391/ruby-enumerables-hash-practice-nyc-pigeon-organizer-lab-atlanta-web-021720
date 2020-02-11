e159aabe74ec947d6a033dc25d15c50ce8c928f2
Gene Campbell III <gcampbell391@gmail.com>
2020-02-11 21:03:52 +0000

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,21 @@
 def nyc_pigeon_organizer(data)
-  # write your code here!
+  pigeons = {}
+  data.each do |property, hash|
+    hash.each do |attribute, array|
+      array.each do |name|
+        if !pigeons.has_key?(name)
+          pigeons[name] = {}
+        end
+
+        if !pigeons[name].has_key?(property)
+          pigeons[name][property] = []
+        end
+
+        if !pigeons[name][property].include?(attribute)
+          pigeons[name][property] << attribute.to_s
+        end
+      end
+    end
+  end
+  pigeons
 end
